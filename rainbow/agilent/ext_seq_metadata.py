203bf4a2020eefa65643eb2c3a6ba48edb4277e9
OctaneOolong <62565951+OctaneOolong@users.noreply.github.com>
2023-06-27 09:54:14 +1000

    add comments

--- rainbow/agilent/ext_seq_metadata.py
+++ rainbow/agilent/ext_seq_metadata.py
@@ -6,10 +6,11 @@ Module containing core function extract_sequence_metadata and dependencies
 from lxml import etree
 
 
-def extract_sequence_metadata(filepath: str):
+def extract_sequence_metadata(filepath: str) -> dict[str, str]:
     """
-    For a given .xml file "filepath" and dictionary of relative xpaths starting from
-    "/Doc/Content", return a dictonary of extracted metadata.
+    Take a sequence.acam_ file and return a dictionary of extracted metadata.
+    
+    Elements are specified by a relative xpath starting from the branch at "/Doc/Content". Place the xpath in xpath_dict
     """
     
     xpath_dict = get_xpath_dict()
@@ -20,17 +21,21 @@ def extract_sequence_metadata(filepath: str):
 
     tree = etree.parse(filepath)  # create the etree object
     root = tree.getroot()
-
+    
+    # get the namespace for the file from the root tag
     namespace = root.tag.split("}")[0].strip(
         "{"
-    )  # get the namespace for the file from the root tag
+    )
     ns = {"acaml": namespace}
 
+    # iterate through the xpath_dict trying to get each element
     for description, rel_path in xpath_dict.items():
         try:
             # construct xpaths using the defined namespace
             xpath_exp = xpath_factory(rel_path)
+            
             # get the result of the xpath exp as a list
+            
             result = root.xpath(xpath_exp, namespaces=ns)
             # extract the item text from results list
             # each item is <class 'lxml.etree._Element'>
@@ -38,6 +43,7 @@ def extract_sequence_metadata(filepath: str):
             if len(result) < 2:
                 seq_metadata[description] = result[0].text
             else:
+                # unpack the list into indexed keys
                 for idx, item in enumerate(result):
                     # remove original unindexed key
                     seq_metadata.pop(description)
@@ -50,6 +56,12 @@ def extract_sequence_metadata(filepath: str):
 
 
 def get_xpath_dict():
+    """
+    In the .acam* files every metadata element lies on the /Doc/Content/ root,
+    branching from that point. The necessary xpath is complicated, needing a namespace
+    injection at every node, so for ease of use place a simple relative xpath here and
+    the factory will do the rest.
+    """
     xpath_dict = {
             "seq_name": "/SampleContextParams/IdentParam/Name",
             "vialnum": "/SampleParams/AcqParam/VialNumber",
@@ -59,6 +71,10 @@ def get_xpath_dict():
 
 
 def xpath_factory(rel_path: str):
+    """
+    For the root path specified in extract_sequence_metadata and relative paths in
+    xpath_dict, construct a full xpath expression with the necessary namspace.
+    """
     namespace_inj = "/acaml:"
     path_root = "./"
     common_path = "/Doc/Content"
